96167b91b4de16b27a36e69b1acac353d549f864
cmindrelaxing <shakil.devcraft@gmail.com>
2024-01-28 16:49:55 +0600

    Programming Course

---
+++ milestone-7/fundamentals.js
@@ -0,0 +1,17 @@
+// step 1: How to declare a variable using let and const
+const fatherName = 'Arnold';
+let season = 'rainy';
+season = 'winter';
+
+// console.log(season);
+
+// 6 basic conditions: >, <, ===, !==, <=, >=
+// multiple conditions: &&, ||
+
+if(fatherName === 'Oh no' || season === 'rainy'){
+    // console.log('It is a valid');
+}else if(fatherName === 'Shakil'){
+    // console.log('My father is Shakil');
+}else{
+    // console.log('I am not sure, It is valid or false');
+}
