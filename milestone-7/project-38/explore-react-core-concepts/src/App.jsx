a3c6848f723e5ec80609a92b915c1be4e9bc7f27
cmindrelaxing <shakil.devcraft@gmail.com>
2024-01-31 17:26:11 +0600

    today again start project from number 3 video

--- milestone-7/project-38/explore-react-core-concepts/src/App.jsx
+++ milestone-7/project-38/explore-react-core-concepts/src/App.jsx
@@ -6,8 +6,13 @@ function App() {
   return (
     <div>
       <h1>Welcome, React world</h1>
+      <Person />
     </div>
   )
+}
+
+function Person() {
+  return <h3>I am a Person</h3>
 }
 
 export default App
