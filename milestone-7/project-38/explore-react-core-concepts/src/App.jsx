9351725fbf3ac14bc50981f5ae52218d50ba29cd
cmindrelaxing <shakil.devcraft@gmail.com>
2024-01-31 17:51:15 +0600

    install props validation and use it

--- milestone-7/project-38/explore-react-core-concepts/src/App.jsx
+++ milestone-7/project-38/explore-react-core-concepts/src/App.jsx
@@ -1,18 +1,56 @@
-
+import PropTypes from 'prop-types';
 import './App.css'
 
 function App() {
 
+  
   return (
+    
     <div>
       <h1>Welcome, React world</h1>
       <Person />
+      <Developer />
+      <Device name="Laptop" />
     </div>
   )
 }
 
-function Person() {
-  return <h3>I am a Person</h3>
+function Device(device) {
+  console.log(device);
+  return <h2>This is Device: {device?.name}</h2>
 }
+
+function Person() {
+  const age = 35;
+  const person = {name: 'Shakil'};
+  return (
+    <div className='student'>
+      <h3>I am a {person?.name}, My age is {age}</h3>
+      <p>Name: {person?.name}</p>
+      <p>age: {age}</p>
+    </div>
+  )
+}
+
+function Developer() {
+
+  const developerStyle = {
+    margin: '20px',
+    padding: '20px',
+    border: '2px solid purple',
+    borderRadius: '10px',
+  };
+
+  return (
+    <div style={developerStyle}>
+      <h5>Devo devo</h5>
+      <p>Coding: </p>
+    </div>
+  )
+}
+
+Device.propTypes = {
+  name: PropTypes.string.isRequired,
+};
 
 export default App
