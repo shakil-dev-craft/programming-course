880b046e0bbb352e502ee3ce2827f75087101f61
cmindrelaxing <shakil.devcraft@gmail.com>
2024-02-05 18:40:25 +0600

    new routes added

--- milestone-8/introduction-to-react-router/src/components/Header/Header.jsx
+++ milestone-8/introduction-to-react-router/src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <div>
             <h1>This is Navbar</h1>
-            <nav>
-                <a href="/">Home</a>
-                <a href="/about">About</a>
-                <a href="/contact">Contact</a>
+            <nav className="flex gap-5">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/users">Users</Link>
             </nav>
         </div>
     );
