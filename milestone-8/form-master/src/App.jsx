6413815f8f39450195f0f6d78c1ab80d7701b541
cmindrelaxing <shakil.devcraft@gmail.com>
2024-02-09 16:26:17 +0600

    How to use useRef with form learn it by video 46.3

--- milestone-8/form-master/src/App.jsx
+++ milestone-8/form-master/src/App.jsx
@@ -1,15 +1,17 @@
 
 import './App.css'
-import StatefulForm from './components/StatefulForm/StatefulForm'
+import RefForm from './components/RefForm/RefForm'
+// import StatefulForm from './components/StatefulForm/StatefulForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 
 function App() {
 
   return (
     <>
-      <h1 className='mb-5'>Form Master</h1>
+      <h1 className='mb-5 font-medium'>Form Master</h1>
       {/* <SimpleForm /> */}
-      <StatefulForm />
+      {/* <StatefulForm /> */}
+      <RefForm />
     </>
   )
 }
---
+++ milestone-8/form-master/src/components/RefForm/RefForm.jsx
@@ -0,0 +1,44 @@
+import { useRef, useState } from "react";
+
+const RefForm = () => {
+
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    // validate password
+    const [error, setError] = useState();
+
+    const handleRefForm = (e) => {
+        e.preventDefault();
+        if(passwordRef?.current?.value?.length < 6) {
+            setError('Please enter a password at least 6 characters');
+        }else{
+            console.log(nameRef?.current?.value);
+            console.log(emailRef?.current?.value);
+            console.log(passwordRef?.current?.value);
+            setError('');
+        }
+    };
+
+    console.log(error);
+
+    return (
+        <div>
+            <form onSubmit={handleRefForm}>
+                <input ref={nameRef} className="outline-none border mb-5 rounded-full px-4 py-1" type="text" name="name" placeholder="Type your name ..." />
+                <br />
+                <input ref={emailRef} className="outline-none border mb-5 rounded-full px-4 py-1" type="email" name="email" id="" placeholder="Type your email ..." />
+                <br />
+                <input ref={passwordRef} className="outline-none border mb-5 rounded-full px-4 py-1" type="password" name="password" id="password" placeholder="Password" required />
+                {
+                    error && <p>{error}</p>
+                }
+                <br />
+                <button type="btn" className="bg-red-600 text-white py-1 px-5 rounded-full">Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default RefForm;
